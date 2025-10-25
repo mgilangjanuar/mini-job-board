15670569660a7c3d77034ec491a94ed7a652f79a
M Gilang Januar <mgilangjanuar@gmail.com>
2025-10-25 20:00:45 +0700

    Add Supabase auth redirects in layouts
    Perform server-side user checks in auth and dashboard layouts. Auth
    layout now redirects

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,18 @@
-export default function AuthLayout({
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+
+  if (data.user) {
+    return redirect("/dashboard");
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">{children}</div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,8 +7,7 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }>) {
   const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
-  console.log(data, error);
+  const { error } = await supabase.auth.getUser();
 
   if (error) {
     return redirect("/auth");
