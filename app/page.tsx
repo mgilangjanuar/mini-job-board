90df85787e2a97445612ec83f539a16cb37c8a2a
M Gilang Januar <mgilangjanuar@gmail.com>
2025-10-26 19:56:24 +0700

    Add job list provider and search input

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import JobList from "@/components/job/list";
+import { Input } from "@/components/ui/input";
+import { JobListProvider, useJobList } from "@/hooks/job/use-list";
+import { SearchIcon } from "lucide-react";
+
 export default function Home() {
-  return <div className="py-6"></div>;
+  return (
+    <JobListProvider>
+      <HomePage />
+    </JobListProvider>
+  );
+}
+
+function HomePage() {
+  const { search, setSearch } = useJobList();
+
+  return (
+    <div className="py-6 space-y-6">
+      <div className="relative flex-1">
+        <SearchIcon className="size-4 absolute top-2.5 left-2.5 text-muted-foreground" />
+        <Input
+          placeholder="Search job..."
+          value={search || ""}
+          onChange={(e) => setSearch?.(e.target.value)}
+          className="pl-9"
+        />
+      </div>
+      <JobList />
+    </div>
+  );
 }
